5182875fe2b5703de36151b0d599384246e685a5
hyesungoh <haesungoh414@gmail.com>
2020-09-08 15:44:02 +0900

    request detail of heritage, get content, img url

--- heritage_api.py
+++ heritage_api.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 search_name = '숭례문'
+# search_name = '화성'
+# search_name = input()
+
 url_list = 'http://www.cha.go.kr/cha/SearchKindOpenapiList.do?pageUnit=30&ccbaMnm1=' + search_name
 # url_search = 'http://www.cha.go.kr/cha/SearchKindOpenapiDt.do?ccbaCtcd=21&ccbaAsno=00&ccbaCtcd=11'
 # ccbaKdcd 종목코드, ccbaAsno 지정번호, ccbaCtcd 시도코드
@@ -17,17 +20,45 @@ for heritage in sp_html:
         temp_dict['문화재명2'] = temp_html.ccbamnm2.text
         temp_dict['위치_도'] = temp_html.ccbactcdnm.text
         temp_dict['위치_시'] = temp_html.ccsiname.text
-        temp_dict['종목코드'] = temp_html.ccbakdcd.text
-        temp_dict['시도코드'] = temp_html.ccbactcd.text
-        temp_dict['지정번호'] = temp_html.ccbaasno.text
+
+        temp_kbcd = temp_html.ccbakdcd.text
+        temp_dict['종목코드'] = temp_kbcd
+
+        temp_ctcd = temp_html.ccbactcd.text
+        temp_dict['시도코드'] = temp_ctcd
+
+        temp_anno = temp_html.ccbaasno.text
+        temp_dict['지정번호'] = temp_anno
+
+        temp_require = 'ccbaKdcd=' + temp_kbcd + '&ccbaCtcd=' + temp_ctcd + '&ccbaAsno=' + temp_anno
+
+        # img_url = 'http://www.cha.go.kr/cha/SearchImageOpenapi.do?' + temp_require
+        # img = requests.get(img_url)
+        # sp_img = img.text.split('<sn>')
+        # parsed_img = BeautifulSoup(sp_img[1], 'html.parser')
+        # temp_dict['이미지'] = parsed_img.imageurl.text
+
+        detail_url = 'http://www.cha.go.kr/cha/SearchKindOpenapiDt.do?' + temp_require
+
+        detail = requests.get(detail_url)
+        parsed_detail = BeautifulSoup(detail.text, 'html.parser')
+        temp_dict['이미지'] = parsed_detail.imageurl.text
+        temp_dict['내용'] = parsed_detail.content.text
+
+
         searched_list.append(temp_dict)
     except:
         pass
 
 print('검색어 : ', search_name)
 print('몇개임? : ', len(searched_list))
+
 for i in searched_list:
-    print(i)
+    for key, value in i.items():
+        print(key, ': ', value)
+
+print('\n\n이렇게 들어가 있음 : \n', searched_list[0])
+
 
 # print(searched_list)
 # print(html.text)

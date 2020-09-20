3c8bdc0efdd87f28680a7b0b6746f46712c5a86d
hyesungoh <haesungoh414@gmail.com>
2020-09-20 17:55:59 +0900

    file write by dict

--- heritage_api.py
+++ heritage_api.py
@@ -8,8 +8,8 @@ search_name = '화성'
 f = open("heritage.txt", 'a')
 
 
-url_list = 'http://www.cha.go.kr/cha/SearchKindOpenapiList.do?pageUnit=3&ccbaMnm1=' + search_name
-# url_list = 'http://www.cha.go.kr/cha/SearchKindOpenapiList.do'
+# url_list = 'http://www.cha.go.kr/cha/SearchKindOpenapiList.do?pageUnit=3&ccbaMnm1=' + search_name
+url_list = 'http://www.cha.go.kr/cha/SearchKindOpenapiList.do?pageUnit=15944'
 # url_search = 'http://www.cha.go.kr/cha/SearchKindOpenapiDt.do?ccbaCtcd=21&ccbaAsno=00&ccbaCtcd=11'
 # ccbaKdcd 종목코드, ccbaAsno 지정번호, ccbaCtcd 시도코드
 html = requests.get(url_list)
@@ -17,6 +17,8 @@ html = requests.get(url_list)
 # print(html.text)
 sp_html = html.text.split('<sn>')
 searched_list = []
+
+progress = 1
 for heritage in sp_html:
     temp_html = BeautifulSoup(heritage, 'html.parser')
     try:
@@ -49,9 +51,13 @@ for heritage in sp_html:
         parsed_detail = BeautifulSoup(detail.text, 'html.parser')
         temp_dict['이미지'] = parsed_detail.imageurl.text
         temp_dict['내용'] = parsed_detail.content.text
-
-
+        temp_dict['시대'] = parsed_detail.cccename.text
+        temp_dict['경도'] = parsed_detail.longitude.text
+        temp_dict['위도'] = parsed_detail.latitude.text
         searched_list.append(temp_dict)
+
+        print('현재 진행상황 : ', progress)
+        progress += 1
     except:
         pass
 
@@ -60,13 +66,14 @@ print('몇개임? : ', len(searched_list))
 
 for i in searched_list:
     for key, value in i.items():
-        txt_data = key + ':' + value
+        txt_data = key + ':' + value + '\n'
         print(txt_data)
-        f.write(txt_data)
+        # f.write(txt_data)
 
+f.write(str(searched_list))
 f.close()
 
-print('\n\n이렇게 들어가 있음 : \n', searched_list[0])
+# print('\n\n이렇게 들어가 있음 : \n', searched_list[0])
 
 
 # print(searched_list)
